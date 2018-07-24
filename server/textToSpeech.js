285dc26aff01f0c65c803065736ed3eb4a6d4909
Georgina Lopez <30646006+georgina108@users.noreply.github.com>
2018-07-24 12:13:19 -0700

    Update textToSpeech.js

--- server/textToSpeech.js
+++ server/textToSpeech.js
@@ -3,9 +3,6 @@ const fs = require('fs');
 
 const client = new textToSpeech.TextToSpeechClient()
 
-/**
- * TODO(developer): Uncomment the following lines before running the sample.
- */
 const text = "This breathing awareness relaxation script will guide you to focus on each stage of a breath as you breathe slowly and gently.Let's begin. Throughout this breathing awareness exercise, breathe in this way:Breathe in to the count of four, hold for the count of 3, and breathe out to the count of 5. It goes like this: Breathe in...2....3....4..... hold...2...3....exhale...2....3....4....5...Breathe in...2....3....4..... hold...2...3....exhale...2....3....4....5...Breathe in...2....3....4..... hold...2...3....exhale...2....3....4....5...Breathe in...2....3....4..... hold...2...3....exhale...2....3....4....5...Continue to breathe at this slow pace.While you are breathing slowly, I'll direct your breathing awareness to different stages of the breath. Focus all of your attention on each stage I mention.First, notice the breath as it enters your nose. Notice each time you breathe in, the way the breath feels on your nostrils.Feel the breath as it passes through your nasal passages, and down behind your throat."
 const outputFile = './output2.mp3';
 
